eab343b08352dd7220f2befff3989dde6e47ae1a
argatanjung165 <arga.tanjung165@gmail.com>
2021-04-29 11:14:17 +0700

    assignment3

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import ExchangeRate from "./assignment";
+
+function App() {
+  return (
+    <div>
+      <ExchangeRate />
+    </div>
+  );
+}
+
+export default App;
